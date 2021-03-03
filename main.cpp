a2c1a16bee825dc787872a553f9ac1f318148cc2
Samuel GOLDENBERG <samuel.goldenberg@epsi.fr>
2021-03-03 17:39:50 +0100

    Modification des propriétés du projet pour résoudre un bug de débogage lié à des librairies manquantes Annotations du code

--- main.cpp
+++ main.cpp
@@ -12,12 +12,13 @@ using namespace std;
 int main() {
 
 
-	VideoCapture cap(0);
-	Mat img;
-	while (true) {
+	VideoCapture cap(0);		   //Choix de la source de la vidéo (ici cap(0) signifie que la vidéo vient de la caméra)
+	Mat img;					   //Création d'une matrice pour afficher et traiter la vidéo
+	
+	while (true) {				   //Affichage de l'image obtenue
 
-		cap.read(img);
-		imshow("Rendu", img);
-		waitKey(1);
+		cap.read(img);			   //Indique l'image de sortie
+		imshow("Rendu", img);	   //Affiche une fenêtre contenant img et de nom "Rendu"
+		waitKey(1);				   //Délai d'affichage, ici l'image est mise à jour dans la fenêtre toute les 1 millisecondes
 	}
 }
